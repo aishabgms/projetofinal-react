2d7f905732dd76e685d33fb4fa3a4af032ddf6f7
Aisha Brito <aishabrito49@gmail.com>
2024-02-15 21:04:55 -0300

    atualizando

---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from './footer.module.css'
+
+function Footer() {
+  return(
+    <footer className={styles.footer}>
+      <p>
+        Criado por Aisha Brito 🍭
+
+
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
